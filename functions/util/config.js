c879deaff2bec4637bb73b58039a8abf38c85eb4
Alvaro <alvaro.serrano@wmich.edu>
2020-05-06 12:19:33 +0200

    sec advisory

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: "AIzaSyBum5c6b6WGilxwH-3Ni3QUzHcFoWkiipU",
-  authDomain: "socialape-44490.firebaseapp.com",
-  databaseURL: "https://socialape-44490.firebaseio.com",
-  projectId: "socialape-44490",
-  storageBucket: "socialape-44490.appspot.com",
-  messagingSenderId: "558521782897",
-  appId: "1:558521782897:web:c84c38f19f1a952f"
+  apiKey: process.env.API_KEY,
+  authDomain: 'socialape-44490.firebaseapp.com',
+  databaseURL: 'https://socialape-44490.firebaseio.com',
+  projectId: 'socialape-44490',
+  storageBucket: 'socialape-44490.appspot.com',
+  messagingSenderId: '558521782897',
+  appId: '1:558521782897:web:c84c38f19f1a952f'
 };
